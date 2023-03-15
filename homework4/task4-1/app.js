9c5bb16bf95bdc6859db74f583a2c6f738874704
Damon Du <damon_du@epam.com>
2023-03-15 16:03:43 +0800

    homework4

---
+++ homework4/task4-1/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRouter from './src/routes/user.router';
+import groupRouter from './src/routes/group.router';
+import swaggerInstall from './utils/swagger';
+
+const app = express();
+
+swaggerInstall(app);
+
+//配置body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//配置art-template
+app.engine('html', require('express-art-template'));
+
+//配置路由
+app.use('/user', userRouter);
+app.use('/group',groupRouter)
+
+app.listen(3000, () => {
+  console.log(
+    'port is 3000\nPlease visit http://127.0.0.1:3000/swagger for API document'
+  );
+});
---
+++ homework4/task4-1/src/routes/user.router.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} from '../controller/user.controller';
+
+const router = express.Router();
+
+router.get('/', getAllUser);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/', updateUser);
+router.delete('/', deleteUser);
+
+export default router;
